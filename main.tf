9b65f30a58739b69eb5e74af8d4239f24c51dfa8
masoodazam <140827933+masoodazam@users.noreply.github.com>
2023-09-12 16:15:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "local_file" "pet" {
-filename = "/root/pets.txt"
-content = "we love pets!"
+  filename = "/root/pets.txt"
+  content  = "we love pets!"
+}
+
+provisioner "local-exec" {
+  command = "sudo chown root:root /root/pets.txt && sudo chmod 644 /root/pets.txt"
 }
